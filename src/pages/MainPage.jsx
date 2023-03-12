f44cf8df079ac4cf827af6c835bfc1438cf69253
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-03-12 15:38:47 +0200

    Update MainPage layout

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Button from '../components/Button'
 
+import './MainPage.sass'
+
 function MainPage() {
   return (
     <div>
       <h1>Dakh.15 Store Components</h1>
       <p>This is the project cotaining React components for Dakh.15 store.</p>
-      <Button className="btn-primary" href="/Buttons">See buttons</Button>
-      <Button className="btn-secondary" href="/Icons">See icons</Button>
+      <div className="buttons">
+        <Button className="btn-primary" href="/Buttons">See buttons</Button>
+        <Button className="btn-secondary" href="/Icons">See icons</Button>
+      </div>
     </div>
   )
 }
