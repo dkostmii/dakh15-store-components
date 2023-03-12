2427d165a914b830caab5a6f5a463355f5bcd581
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-03-12 16:52:56 +0200

    Add burger icon

---
+++ src/components/IconBurger.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+import { ReactComponent as Burger } from '../icons/burger.svg'
+
+import './IconBurger.sass'
+
+function IconBurger() {
+    const [active, setActive] = useState(false)
+
+    const className = 'burger' + (active ? ' active' : '')
+
+    const clickListener = () => {
+        setActive(!active)
+    }
+
+    return (
+        <Burger className={className} onClick={clickListener} />
+    )
+}
+
+export default IconBurger
